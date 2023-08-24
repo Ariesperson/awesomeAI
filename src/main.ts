7ea1a2004a99ed61d7b45e94ce15d06a0b2428f5
siyansychen <siyansychen@tencent.com>
2023-08-24 15:19:09 +0800

    feat: 完善整体app相关全局状态，包括主题语言等设置

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import App from './App.vue'
 import { initStore } from "./store";
 const initApp = async () => {
    const app =  createApp(App)
+   
    // 设置项目中的全局状态管理
     initStore(app)
+
     app.mount('#app')
 }
 initApp()
--- src/store/modules/app/index.ts
+++ src/store/modules/app/index.ts
@@ -1,15 +1,39 @@
 import { defineStore } from 'pinia'
 import type { AppState, Language, Theme } from './helper'
 import { getLocalSetting, setLocalSetting } from './helper'
+import { store } from '../../helper'
 
-const useAppstore = defineStore('app-store',{
-    state() {
-        AppState => getLocalSetting()
-    },
+// 使用 defineStore 方法创建应用状态存储对象
+export const useAppStore = defineStore('app-store',{
+     // 通过提供一个状态对象来定义该存储对象的初始状态
+    state: (): AppState => getLocalSetting(), // 从本地存储中读取应用的状态
+    // 添加Actions
     actions:{
-
+        // 设置主题
+        setTheme(theme: Theme) {
+            this.theme = theme
+            this.recordState()
+        },
+        // 设置语言
+        setLanguage(language: Language) {
+            if (this.language !== language) {
+              this.language = language
+              this.recordState()
+            }
+        },
+        // 设置侧边栏折叠状态
+        setSiderCollapsed(collapsed: boolean) {
+            this.siderCollapsed = collapsed
+            // 保存当前的应用状态到本地存储
+            this.recordState()
+        },
+        // 保存当前的应用状态到本地存储
+        recordState() {
+            setLocalSetting(this.$state)
+        },
     },
-
 })
-
-export useAppstore
+// 创建一个注入该应用状态存储对象的高阶组件
+export function useAppStoreWithOut() {
+    return useAppStore(store)
+}
