374b5387d46889edd32db08eb04f253832b23da4
siyansychen <siyansychen@tencent.com>
2023-08-24 15:00:12 +0800

    feat:入口初始化实现

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import { initStore } from "./store";
+const initApp = async () => {
+   const app =  createApp(App)
+   // 设置项目中的全局状态管理
+    initStore(app)
+    app.mount('#app')
+}
+initApp()
