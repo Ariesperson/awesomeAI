afd89b0c444bfff94cba8c87fcfaf24c7a721811
gulugulu <969939390@qq.com>
2023-09-04 15:15:04 +0800

    feat:个人信息展示栏

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -23,5 +23,16 @@ const userInfo = computed(() => userStore.userInfo)
                 <a-avatar size="large" round :src="defaultAvatar" />
             </template>
         </div>
+        <div class="flex-1 min-w-0 ml-2">
+            <h2 class="overflow-hidden font-bold text-md text-ellipsis whitespace-nowrap">
+                {{ userInfo?.name || 'siyanchen' }}
+            </h2>
+            <p class=" overflow-hidden text-xs text-green-500 text-ellipsis whitespace-nowrap">
+                <span
+                    v-if="isString(userInfo.description) && userInfo.description !== ''"
+                    v-html="userInfo.description"
+                />
+            </p>
+        </div>
     </div>
 </template>
